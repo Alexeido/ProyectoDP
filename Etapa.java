01943152e3899673e5cfe26fa4019237e70788e6
Raúl Martín-Romo Sánchez <rmartinrw@alumnos.unex.es>
2022-10-20 09:22:01 +0200

    hola

--- Etapa.java
+++ Etapa.java
@@ -8,7 +8,9 @@
 public class Etapa
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int distancia;
+    private int dificultad;
+    private String nombre;
 
     /**
      * Constructor for objects of class Etapa
@@ -16,18 +18,19 @@ public class Etapa
     public Etapa()
     {
         // initialise instance variables
-        x = 0;
+        distancia=0;
+        dificultad=0;
+        nombre="";
     }
-
+    
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Constructor for objects of class Etapa
      */
-    public int sampleMethod(int y)
+    public Etapa(int distancia, int dificultad, String nombre)
     {
-        // put your code here
-        return x + y;
+        // initialise instance variables
+        this.distancia=distancia;
+        this.dificultad=dificultad;
+        this.nombre=nombre;
     }
 }
