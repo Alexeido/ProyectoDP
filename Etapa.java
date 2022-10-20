db076828da978bca0afa3ea0f3deb344e97578ab
Raúl Martín-Romo Sánchez <rmartinrw@alumnos.unex.es>
2022-10-20 10:05:11 +0200

    achus

--- Etapa.java
+++ Etapa.java
@@ -8,6 +8,7 @@
 public class Etapa
 {
     // instance variables - replace the example below with your own
+<<<<<<< HEAD
     private int distancia;
     private double dificultad;
     private String nombre;
@@ -32,5 +33,36 @@ public class Etapa
         this.distancia=distancia;
         this.dificultad=dificultad;
         this.nombre=nombre;
+=======
+    private String nombre;
+    
+    private int dificultad;
+    
+    private double distancia;
+
+    /**
+     * Constructor for objects of class Etapa
+     */
+    public Etapa(String nombre,int  dificultad,int distancia)
+    {
+        this.nombre = nombre;
+        this.dificultad = dificultad;
+        this.distancia = distancia;
+        
+    }
+
+
+    public String getnombre()
+    {
+    return nombre;
+    }
+        public int getdificultad()
+    {
+    return dificultad;
+    }
+        public double getdistancia()
+    {
+    return distancia;
+>>>>>>> 4467a72d13cc086fb7e3b4986a0a882ba3c7babe
     }
 }
